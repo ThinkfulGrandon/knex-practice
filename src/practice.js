d25e8cd6354d9edc617c90b07770993625a1bda2
Grandon Smith <grandon.smith@yahoo.com>
2020-12-23 21:51:51 -0500

    got cmd prompt to work, have tables for amazon and youtube clones

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,13 @@
-console.log('hello, practice');
+require('dotenv').config()
+const knex = require('knex');
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL,
+});
+
+console.log('knex and driver installed');
+
+knexInstance('amazong_products').select('*')
+    .then(result => {
+        console.log(result)
+    });
